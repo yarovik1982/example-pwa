a7c3aeafbe1404853318b77898c453b4531d83d6
acunamatata82 <yar.oleg1982@mail.ru>
2026-03-13 18:06:33 +0300

    update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", event => {
    event.waitUntil(
       caches.open("v1").then(cache => {
-         return cache.addAll(["index.html", "icon.png"]);
+         return cache.addAll(["index.html", "hero.png"]);
       })
    );
 });
